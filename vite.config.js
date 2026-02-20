7df1d32093ba94f2cf928f86ec4010fcaee903eb
hamedAbdollahzade <hamedabdollahzade.ab@gmail.com>
2026-02-20 21:43:19 +0330

    Trying to deploy the project on the GitHub page

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), vue(), vueDevTools()],
-  base: './',
+  base: './vue-shop/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
